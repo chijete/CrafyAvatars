f99ada08786a724fa27e9a3aad583af5ec299a23
Chijete Cuentas <chijeteinc@gmail.com>
2022-12-13 21:53:31 -0300

    Change split for match on Js version

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ require 'lib/CrafyAvatars.php';
 $myClass = new CrafyAvatars();
 
 // clo, mouth, eyes, top
-echo $myClass->generate_svg([
-    '03','15','15','15',
-    '03','03','03','03',
-    '03','01'
-]);
+// echo $myClass->generate_svg([
+//     '03','15','15','15',
+//     '03','03','03','03',
+//     '03','01'
+// ]);
 
 // echo $myClass->convertBaseArrayOnBaseInt([
 //     '03', '15', '15', '15',
@@ -21,6 +21,8 @@ echo $myClass->generate_svg([
 
 // echo $myClass->getAleatoryAvatarID();
 // echo "<br/>";
+
+echo $myClass->generate_svg_byInt('05030500000000000015');
 
 echo ((16*16*16*16)*(4*4*4*4)*(4)*(43)) . ' posibilidades.';
 
